391f09bbdd64bfc556d1016b0d19db459a3b88a4
Faheem Alvi <148661058+FaheemAlvii@users.noreply.github.com>
2026-04-01 00:15:46 +0500

    rebase for rename

---
+++ wlink_whatsapp_crm/py314/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GTS WhatsApp CRM',
+    'version': '17.0.1.0',
+    'summary': 'Send WhatsApp messages to customers directly from CRM.',
+    'description': """
+This module enables sending WhatsApp messages to customers easily from the CRM leads and opportunities.
+Enhance your customer communication directly within Odoo CRM.
+""",
+    'category': 'Sales',
+    'author': 'WLink',
+    'license': 'LGPL-3',
+    'sequence': 1,
+    'images': ['static/description/crm_banner.jpg'],
+    'depends': ['base', 'crm', 'wlink_whatsapp'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/wlink_crm_wizard_view.xml',
+        'views/crm_lead_view.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'wlink_whatsapp/static/src/scss/style.scss',
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
---
+++ wlink_whatsapp_livechat/py313/__manifest__.py
@@ -0,0 +1,27 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'WhatsApp Live Chat (Discuss Integration)',
+    'version': '17.0.0.1.0',
+    'license': 'OPL-1',
+    'author': "WLink",
+    'summary': 'Chat with customers on WhatsApp directly from Odoo Discuss in real time.',
+    'description': """This module allows Odoo users to chat live with customers on WhatsApp directly from the Discuss app. Seamlessly integrates WhatsApp messaging into the internal chat interface for real-time communication with external contacts.""",
+    'category': 'Extra Tools',
+    'sequence': 1,
+    'images': ['static/description/livechat_banner.jpg'],
+    'depends': [
+        'im_livechat',
+        'mail',
+        'wlink_whatsapp',
+    ],
+    'data': [
+        'views/mail_message.xml',
+    ],
+    'external_dependencies': {'python': ['phonenumbers']},
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 0,
+    'currency': 'EUR',
+}
